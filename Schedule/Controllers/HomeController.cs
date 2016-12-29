9d3b436b7257f91c2f5ed847bdd8e154d3bac3ae
ayalayan <ayalag@grts.co.il>
2016-12-29 12:58:08 +0200

    try commit

--- Schedule/Controllers/HomeController.cs
+++ Schedule/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace Schedule.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            var i = 1;
             return View();
         }
     }
